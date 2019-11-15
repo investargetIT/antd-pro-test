0e80670dedc7c6fcd7883c76da93ac5d8c33e055
InvesTarget <system@investarget.com>
2019-11-15 14:08:03 +0800

    Almost finish about us page

--- js/about_us.js
+++ js/about_us.js
@@ -1,4 +1,12 @@
 const nav = document.getElementById('nav-bar__about-us');
+const animationTargets = document.getElementsByClassName('parallax-animation__container');
+for (let index = 0; index < animationTargets.length; index++) {
+  const elementTarget = animationTargets[index];
+  if (window.innerHeight> (elementTarget.offsetTop + elementTarget.offsetHeight)) {
+    const childTarget = elementTarget.getElementsByClassName('parallax-animation-ready');
+    childTarget[0].classList.add('parallax-animation');
+  }
+}
 window.onscroll = function () {
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   if (scrollTop > 0) {
@@ -6,4 +14,13 @@ window.onscroll = function () {
   } else {
     nav.classList.remove('header-about-bg');
   }
+ 
+  for (let index = 0; index < animationTargets.length; index++) {
+    const elementTarget = animationTargets[index];
+    if (scrollTop + window.innerHeight> (elementTarget.offsetTop + elementTarget.offsetHeight)) {
+      const childTarget = elementTarget.getElementsByClassName('parallax-animation-ready');
+      childTarget[0].classList.add('parallax-animation');
+    }
+  }
+  
 }
